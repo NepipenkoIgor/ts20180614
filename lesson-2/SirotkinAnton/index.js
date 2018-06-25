545055e8ebf4ca2dd2b86f01d099d224faa503db
Антон Сироткин <a.sirotkin@tradesoft.ru>
2018-06-25 16:46:59 +0300

    4 задание

--- lesson-2/SirotkinAnton/index.js
+++ lesson-2/SirotkinAnton/index.js
@@ -1,11 +1,13 @@
 "use strict";
+// 1
 function isInArray() {
     var arg = [];
     for (var _i = 0; _i < arguments.length; _i++) {
         arg[_i] = arguments[_i];
     }
-    return arg[0].every(function (item) { return arg.splice(1).includes(item); });
+    return arg.shift().every(function (item) { return arg.includes(item); });
 }
+// 2
 function summator() {
     var arg = [];
     for (var _i = 0; _i < arguments.length; _i++) {
@@ -13,6 +15,7 @@ function summator() {
     }
     return arg.reduce(function (item, summ) { return summ + parseInt(item, 10); }, 0);
 }
+// 3
 function getUnique() {
     var arg = [];
     for (var _i = 0; _i < arguments.length; _i++) {
@@ -21,4 +24,20 @@ function getUnique() {
     var unique = new Set();
     arg.forEach(function (item) { return (unique.add(item)); });
     return Array.from(unique);
+}
+function revertWord(word) {
+    var start = word.split('');
+    var reg = /^[a-zA-Z]+$/;
+    var toRevert = start.filter(function (letter) { return reg.test(letter); }).reverse();
+    var i = 0;
+    return start.map(function (letter) {
+        if (reg.test(letter)) {
+            return toRevert[i++];
+        }
+        return letter;
+    }).join('');
+}
+// 4
+function revertSugWords(sug) {
+    return sug.split(' ').map(function (word) { return revertWord(word); }).join(' ');
 }
--- lesson-2/SirotkinAnton/index.ts
+++ lesson-2/SirotkinAnton/index.ts
@@ -1,14 +1,36 @@
 
+// 1
 function isInArray(...arg: any[]): Boolean {
-  return arg[0].every((item: any) => arg.splice(1).includes(item));
+  return arg.shift().every((item: any) => arg.includes(item));
 }
 
+// 2
 function summator(...arg: any[]): number {
   return arg.reduce((item: any, summ: number) => summ + parseInt(item, 10), 0);
 }
 
+// 3
 function getUnique(...arg: any[]) {
   const unique: Set<any> = new Set();
   arg.forEach(item => (unique.add(item)));
   return Array.from(unique);
 }
+
+function revertWord(word: string): string {
+  const start: string[] = word.split('')
+  const reg: RegExp = /^[a-zA-Z]+$/;
+  const toRevert: string[] = start.filter(letter => reg.test(letter)).reverse();
+  let i = 0;
+  return start.map((letter) => {
+    if (reg.test(letter)) {
+      return toRevert[i++];
+    }
+    return letter;
+  }).join('');
+}
+
+// 4
+function revertSugWords(sug: string): string {
+  return sug.split(' ').map(word => revertWord(word)).join(' ');
+}
+
