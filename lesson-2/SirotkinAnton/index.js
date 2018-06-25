1f6eaec08b83d92fa724e74f0a6b0c4d96168de7
kln4a <a.sirotkin@tradesoft.ru>
2018-06-25 06:38:35 +0300

    summator

--- lesson-2/SirotkinAnton/index.js
+++ lesson-2/SirotkinAnton/index.js
@@ -6,3 +6,10 @@ function isInArray() {
     }
     return arg[0].every(function (item) { return arg.splice(1).includes(item); });
 }
+function summator() {
+    var arg = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        arg[_i] = arguments[_i];
+    }
+    return arg.reduce(function (item, summ) { return summ + parseInt(item, 10); }, 0);
+}
--- lesson-2/SirotkinAnton/index.ts
+++ lesson-2/SirotkinAnton/index.ts
@@ -2,3 +2,7 @@
 function isInArray(...arg: any[]) {
   return arg[0].every((item: any) => arg.splice(1).includes(item));
 }
+
+function summator(...arg: any[]) {
+  return arg.reduce((item, summ) => summ + parseInt(item, 10), 0);
+}
